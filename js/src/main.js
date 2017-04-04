e6f25f14ef7b0b3e714dbfca231fe94c69fdb4f8
Michael George <mdgeorge@cs.cornell.edu>
2017-04-03 22:43:13 -0400

    ui tweaks

--- js/src/main.js
+++ js/src/main.js
@@ -6,14 +6,30 @@ requirejs.config({
   },
 });
 
-require(['re', 'lib/insert', 'lib/domReady!'],
-function(RE,   insertAtCaret, doc) {
+require(['re', 'lib/domReady!'],
+function(RE,   doc) {
 
 var process  = doc.getElementById('process');
 var regex    = doc.getElementById('regex');
 var tree     = doc.getElementById('tree');
 var pretty   = doc.getElementById('pretty');
 var examples = doc.getElementById('examples');
+
+var emptySet = doc.getElementById('∅');
+var emptyStr = doc.getElementById('ε');
+
+function insert(input, str) {
+  input.focus();
+  var n = input.selectionStart;
+  var front = input.value.slice(0,input.selectionStart);
+  var back  = input.value.slice(input.selectionEnd);
+  input.value = front + str + back;
+  input.selectionStart = n+1;
+  input.selectionEnd   = n+1;
+}
+
+emptySet.onclick = function() { insert(regex, '∅'); };
+emptyStr.onclick = function() { insert(regex, 'ε'); };
 
 process.onclick = function() {
   try {
@@ -28,6 +44,7 @@ process.onclick = function() {
     while (examples.hasChildNodes())
       examples.removeChild(examples.firstChild);
     var e = re.examples(10);
+    e.sort();
     for (var i in e) {
       var li = doc.createElement('li');
       li.appendChild(doc.createTextNode('"' + e[i] + '"'));
